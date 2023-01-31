14d856af73af28349b908580249ae4aa484d211c
Kyle B. Lackinger <67078189+kbl40@users.noreply.github.com>
2023-01-31 01:46:14 -0500

    test commit for new branch

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
-// import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 // import request from 'request'
 // import { Cheerio } from 'cheerio'
